ba343bb167e72fe67098013296c8c5c08f9f3878
Amit Dusane <amitd@synerzipune.local>
2020-01-13 17:10:59 +0530

    Initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <b-nav tabs>
+      <b-nav-item disabled><b>Smart Quiz</b></b-nav-item>
+      <b-nav-item disabled>Answered {{correctAnswers}}/{{totalAnswers}}</b-nav-item>      
+    </b-nav>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        correctAnswers: Object,
+        totalAnswers: Object
+    }
+}
+</script>
---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,58 @@
+""}{
+        selectedIndex: null,
+        correctIndex: 0
+    }
+  },
+  watch: {
+      currentQuestion() {
+          this.selectedIndex = null;
+      }
+  },
+  methods: {
+      selectedAnswer(index) {
+        this.selectedIndex = index
+      },
+      submitAnswer() {
+          let isCorrect = false
+          if(this.selectedIndex === this.correctIndex){
+              isCorrect = true
+          }
+          this.increamentCorrect(isCorrect)
+      }
+  },
+  computed: {
+    answers() {
+      let answers = [...this.currentQuestion.incorrect_answers]
+      answers.push(this.currentQuestion.correct_answer)
+      console.log(answers.indexOf(this.currentQuestion.correct_answer))
+      return _.shuffle(answers)
+    },
+    queIndex() {
+        return this.questionIndex++
+    }
+  }  
+};
+</script>
+
+<style scoped>
+.list-group {
+  margin-bottom: 15px;
+}
+.list-group-item:hover {
+  background-color: #EEE;
+  cursor: pointer;
+}
+.btn {
+  margin: 0 5px;
+}
+.selected {
+    background-color: lightblue
+}
+.correctAnswer {
+    background-color: lightgreen
+}
+.wrongAnswer {
+    background-color: red
+}
+
+</style>
